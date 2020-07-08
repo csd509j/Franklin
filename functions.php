8f338e11ba43b6d29fa8104ce1fd23223a62aebd
Josh Armentano <josh@abidewebdesign.com>
2020-07-08 07:13:23 -0700

    no message

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 /*
  * Theme update checker
  *
- * @since Hoover Elementary School 1.0
+ * @since Franklin K-8 School 1.0
  */
 require WP_CONTENT_DIR . '/plugins/plugin-update-checker-master/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/csd509j/Hoover',
+	'https://github.com/csd509j/Franklin',
 	__FILE__,
-	'Hoover'
+	'Franklin'
 );
 
 $myUpdateChecker->setBranch('master'); 
@@ -16,20 +16,20 @@ $myUpdateChecker->setBranch('master');
 /*
  * Setup style sheets
  *
- * @since Hoover Elementary School 1.0
+ * @since Franklin K-8 School 1.0
  */
-function hoover_theme_enqueue_styles() {
+function franklin_theme_enqueue_styles() {
     
 	$parent_style = 'csdschools';
 	$child_theme = wp_get_theme();
 	$parent_theme_version = $child_theme->parent();
 	
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', '', $parent_theme_version->version );
-	wp_enqueue_style( 'hoover-style',
+	wp_enqueue_style( 'franklin-style',
 	    get_stylesheet_directory_uri() . '/style.css',
 	    array( $parent_style ),
 	    wp_get_theme()->get('Version')
 	);
 
 }
-add_action( 'wp_enqueue_scripts', 'hoover_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'franklin_theme_enqueue_styles' );
